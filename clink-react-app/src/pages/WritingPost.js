fe44ca2da3e9fefb268956ed9a0652c691ba8088
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-07-27 16:49:24 +0900

    update Main page

--- clink-react-app/src/pages/WritingPost.js
+++ clink-react-app/src/pages/WritingPost.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
-import '../styles/WritingPost.scss';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import CommunityHeader from '../components/community/CommunityHeader';
-import WritingCategory from '../components/community/WritingCategory';
-import PostTagInput from '../components/community/PostTagInput';
-import axios from 'axios';
-import { Navigate } from 'react-router-dom';
-import { Link } from 'react-bootstrap-icons';
+import React, { useState } from "react";
+import "../styles/WritingPost.scss";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import CommunityHeader from "../components/community/CommunityHeader";
+import WritingCategory from "../components/community/WritingCategory";
+import PostTagInput from "../components/community/PostTagInput";
+import axios from "axios";
 
 export default function WritingPost() {
   const [inputPost, setInputPost] = useState({
     categoryNo: 1,
-    title: '',
-    content: '',
+    title: "",
+    content: "",
     tagList: [],
   });
   const insertPost = () => {
@@ -23,14 +21,14 @@ export default function WritingPost() {
       boardCategoryNo: inputPost.categoryNo,
       hashTagVo: inputPost.tagList,
     };
-    if (params.boardTitle.trim() === '' || params.boardContent.trim() === '') {
-      alert('제목 또는 내용을 입력해주세요!');
+    if (params.boardTitle.trim() === "" || params.boardContent.trim() === "") {
+      alert("제목 또는 내용을 입력해주세요!");
     } else {
-      axios.post('http://localhost:80/insertPost', params);
+      axios.post("http://localhost:80/insertPost", params);
       window.location.href =
-        'http://localhost:3000/community/category/?categoryNo=' +
+        "http://localhost:3000/community/category/?categoryNo=" +
         inputPost.categoryNo +
-        '&&filter=1';
+        "&&filter=1";
     }
     //console.log(inputPost.tagList);
   };
@@ -72,7 +70,7 @@ export default function WritingPost() {
           onClick={() => {
             insertPost();
           }}
-          style={{ width: '80%' }}
+          style={{ width: "80%" }}
         >
           글 작성
         </Button>
