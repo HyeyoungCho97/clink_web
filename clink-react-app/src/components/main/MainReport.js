faba0ea3c5a6cc524c100e06105cf0d36150e136
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-08-07 16:57:42 +0900

    update mainCoin

--- clink-react-app/src/components/main/MainReport.js
+++ clink-react-app/src/components/main/MainReport.js
@@ -17,20 +17,17 @@ const MainReport = ({ data }) => {
           {"\n\n"}
         </b>
         <p style={{ fontSize: "14px", textAlign: "left", fontWeight: 700 }}>
-          총 사용 금액은{" "}
-          <span id="emphasis">{data.sum.toLocaleString()}원</span>이고,{"\n"}
+          총 사용 금액은 <span id="emphasis">{data.sum}원</span>이고,{"\n"}
           {"\n"}이 중{" "}
           <span id="emphasis">{category.category[Number(data.category)]}</span>{" "}
           카테고리가 차지하는 금액 비중이{" "}
           <span id="emphasis">{percent + "%"}</span>를 차지합니다.{"\n"}
           {"\n"}
-          가장 비싼 결제는{" "}
-          <span id="emphasis">{data.high.toLocaleString()}원</span> 이고, 가장
-          저렴한 소비는 <span id="emphasis">{data.low.toLocaleString()}원</span>
+          가장 비싼 결제는 <span id="emphasis">{data.high}원</span> 이고, 가장
+          저렴한 소비는 <span id="emphasis">{data.low}원</span>
           이었습니다{"\n"}
           {"\n"}
-          평균 결제 금액은{" "}
-          <span id="emphasis">{data.avg.toLocaleString()}원</span> 입니다.
+          평균 결제 금액은 <span id="emphasis">{data.avg}원</span> 입니다.
           {"\n"}
         </p>
       </div>
